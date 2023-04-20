cd7599c271d650aa1ef6372aa6c15c8f00b022c2
shibohu <hushibo456@gmail.com>
2023-04-19 23:56:15 -0400

    c

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -44,10 +44,10 @@ module.exports = (sequelize, DataTypes) => {
 
     static associate(models) {
       // define association here
-      User.hasMany(models.Comment, {foreignKey: 'userId'})
-      User.hasMany(models.Cart, {foreignKey: 'userId'})
-      User.hasMany(models.Like, {foreignKey: 'userId'})
       User.hasMany(models.Item, {foreignKey: 'sellerId'})
+      User.hasMany(models.Cart, {foreignKey: 'userId'})
+      User.hasMany(models.Comment, {foreignKey: 'userId'})
+      User.hasMany(models.Like, {foreignKey: 'userId'})
     }
   };
 
