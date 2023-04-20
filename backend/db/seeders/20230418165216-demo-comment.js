297d2ca410d8c5f27b546de916d0ca550e639bea
shibohu <hushibo456@gmail.com>
2023-04-20 01:38:14 -0400

    v

--- backend/db/seeders/20230418165216-demo-comment.js
+++ backend/db/seeders/20230418165216-demo-comment.js
@@ -12,28 +12,28 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       { 
         userId: 1,
-        image: faker.image.food(),
+        image: "https://media.istockphoto.com/id/1254474165/photo/tropical-leaves-abstract-green-leaves-texture-nature-background.jpg?s=612x612&w=0&k=20&c=UQ7bV5XZ5Zeg4Lj5uQlg1FOlhZfmZpJirot754UVXWU=",
         itemId: 1,
         comment: faker.lorem.sentence(),
         rating: 2
       },
       { 
         userId: 2,
-        image: faker.image.food(),
+        image: "https://media.istockphoto.com/id/1254474165/photo/tropical-leaves-abstract-green-leaves-texture-nature-background.jpg?s=612x612&w=0&k=20&c=UQ7bV5XZ5Zeg4Lj5uQlg1FOlhZfmZpJirot754UVXWU=",
         itemId: 1,
         comment: faker.lorem.sentence(),
         rating: 2
       },
       { 
         userId: 1,
-        image: faker.image.food(),
+        image: "https://media.istockphoto.com/id/1254474165/photo/tropical-leaves-abstract-green-leaves-texture-nature-background.jpg?s=612x612&w=0&k=20&c=UQ7bV5XZ5Zeg4Lj5uQlg1FOlhZfmZpJirot754UVXWU=",
         itemId: 2,
         comment: faker.lorem.sentence(),
         rating: 4
       },
       { 
         userId: 1,
-        image: faker.image.food(),
+        image: "https://media.istockphoto.com/id/1254474165/photo/tropical-leaves-abstract-green-leaves-texture-nature-background.jpg?s=612x612&w=0&k=20&c=UQ7bV5XZ5Zeg4Lj5uQlg1FOlhZfmZpJirot754UVXWU=",
         itemId: 2,
         comment: faker.lorem.sentence(),
         rating: 4
