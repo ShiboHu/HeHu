2c97c8d70f2e795f6a63f79d850cb0aa7bb97a55
shibohu <hushibo456@gmail.com>
2023-05-11 13:46:14 -0400

    s

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,7 +3,6 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const itemsRouter = require('./items.js');
-const cartsRouter = require('./carts.js');
 const subCatRouter = require('./sub_categories.js')
 const commentRouter = require('./comment.js');
 const cartItemRouter = require('./cart_items.js');
@@ -20,7 +19,6 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/items', itemsRouter);
-router.use('/carts', cartsRouter);
 router.use('/subcategories', subCatRouter);
 router.use('/comments', commentRouter);
 router.use('/cart-items', cartItemRouter);
